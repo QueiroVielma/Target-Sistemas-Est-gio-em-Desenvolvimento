61152b2dd850235f1a3230b3a960ffa331d6d4c3
QueiroVielma <queigerar@gmail.com>
2024-10-02 13:25:54 -0300

    Funcoes exercicio 2

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ def pertence_fibonacci(num:int)-> bool:
 
 def verifica(num:int)->bool:
     return pertence_fibonacci(5*num*num + 4) or pertence_fibonacci(5*num*num - 4)
+
+def contar_letra_a(texto):
+    contador = 0
+    for caractere in texto:
+        if caractere.lower() == 'a':
+            contador += 1
+
+    if contador > 0:
+        print(f"A letra 'a' aparece {contador} vez(es) na string.")
+    else:
+        print("A letra 'a' não esta na string.")
+
+def achar_a ():
+    frase = "A Amazônia é uma área incrível."
+    contar_letra_a(frase)
+
